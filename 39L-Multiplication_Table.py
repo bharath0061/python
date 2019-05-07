8569bd9d2d24dadca77108d11700eefd52a8526c
Bharath Reddy <bharre01@in.ibm.com>
2019-05-07 22:36:51 +0530

    firsrt script with pycharm using method also with debugging pointer

---
+++ 39L-Multiplication_Table.py
@@ -0,0 +1,9 @@
+def multiplication_table(tab_num, from_num, to_num):
+    print(f"Multiplaction Table of {tab_num}")
+    for i in range(from_num, to_num + 1):
+        print(f"{tab_num} * {i} = {tab_num * i}")
+
+multiplication_table(2, 1, 10)
+multiplication_table(3, 1, 10)
+multiplication_table(4, 1, 10)
+
