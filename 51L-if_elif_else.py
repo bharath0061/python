4cfc28e4904bce756dbe5a3b12af0397624af9e6
Bharath Reddy <bharre01@in.ibm.com>
2019-05-09 16:12:44 +0530

    51 if condition

---
+++ 51L-if_elif_else.py
@@ -0,0 +1,10 @@
+[a, b, c] = [0, 6, 4]
+
+print(a, b, c)
+
+if b==0:
+    print ("The given value it neither even nor odd, it's zero")
+elif b%2==0:
+    print ("The given number is even")
+else:
+    print ("The given number is odd")
