b1bef129da8d14c043ec3e649dd209d7cfde05bd
Bharath Reddy <bharre01@in.ibm.com>
2019-05-09 11:08:03 +0530

    39L added comment for TODO

--- 39L-Multiplication_Table.py
+++ 39L-Multiplication_Table.py
@@ -2,7 +2,7 @@ def multiplication_table(tab_num, from_num, to_num):
     print(f"Multiplaction Table of {tab_num}")
     for i in range(from_num, to_num + 1):
         print(f"{tab_num} * {i} = {tab_num * i}")
-
+# TODO : Make don't take long break while learning something
 multiplication_table(2, 1, 10)
 multiplication_table(3, 1, 10)
 multiplication_table(4, 1, 10)
